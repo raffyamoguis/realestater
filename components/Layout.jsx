083a32ea26ea2b295d56b72811e8f9440283abb1
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-09-29 20:54:54 +0800

    fix optimization issue

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Real Estate</title>
+        <title>RealEstateR</title>
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
